45eec8f4fca319fc42c61b396d38996493cbc2d6
ososmalo <brotherandfrindesgames@gmail.com>
2026-04-23 15:29:12 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-هimport React, { useEffect, useMemo, useState } from "react";
+import React, { useEffect, useMemo, useState } from "react";
 import { AnimatePresence, motion } from "framer-motion";
 import {
   BarChart3,
